658ebf71747e800f9f33c4f4d70cd8b57cd12235
jneo8 <james851212@gmail.com>
2018-03-27 17:01:04 +0800

    Add __init__.py

--- main/app.py
+++ main/app.py
@@ -13,16 +13,6 @@ class APIInfo:
         resp.body = json.dumps(doc, ensure_ascii=False)
         resp.status = falcon.HTTP_200
 
-    def on_post(self, req, resp):
-        """GET Method."""
-        doc = {
-            "version": "1.0",
-            "http_method": req.method,
-        }
-        resp.body = json.dumps(doc, ensure_ascii=False)
-        resp.status = falcon.HTTP_200
-
-
 api = application = falcon.API()
 
 # Addd route
