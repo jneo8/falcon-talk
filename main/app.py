0e8703b4119feecf93df01f237d1a42c496e4993
jneo8 <james851212@gmail.com>
2018-04-13 18:11:33 +0800

    Add version control midderware

--- main/app.py
+++ main/app.py
@@ -1,13 +1,50 @@
+"""App."""
 import falcon
 import json
 
-from .resources import (
-    RESTfulExample,
-    APIInfo,
+from neologger import Logger
+from .resources import v1
+
+logger = Logger(__name__)
+
+
+class AddPostParams():
+    """Get Params from stream & set to params."""
+
+    def process_request(self, req, resp):
+        """Process_request."""
+        body = req.stream.read()
+        post_params = json.loads(body.decode("utf-8"))
+        logger.info(post_params)
+        for k, v in post_params.items():
+            req.params[k] = v
+
+        logger.debug(dir(req))
+
+
+class VersionRouting:
+    """Version control."""
+
+    def process_request(self, req, resp):
+        """Process_request."""
+        version = req.get_header("version")
+        req.path = f"/v{version}{req.path}"
+        if req.path[-1] == "/":
+            req.path = req.path[:-1]
+
+
+api = application = falcon.API(
+    middleware=[
+        AddPostParams(),
+        VersionRouting(),
+    ]
 )
 
-api = application = falcon.API()
 
 # Addd route
-api.add_route("/", APIInfo())
-api.add_route("/restful", RESTfulExample())
+api.add_route("/", v1.APIInfo())
+api.add_route("/restful", v1.RESTfulExample())
+
+
+api.add_route("/v1", v1.APIInfo())
+api.add_route("/v1/restful", v1.RESTfulExample())
